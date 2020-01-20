86375b8038615d3baab1d70e587ef12bb7ccb16c
akgajjar <aniketgajjar966@gmail.com>
2020-01-20 16:54:08 +0530

    Utility Optimization of classes

--- Tuition_Management_System/src/main/java/com/infoobjects/Tuition_Management_System/Utils/Projectutils.java
+++ Tuition_Management_System/src/main/java/com/infoobjects/Tuition_Management_System/Utils/Projectutils.java
@@ -12,139 +12,138 @@ import com.infoobjects.Tuition_Management_System.Service.StudentService;
 import com.infoobjects.Tuition_Management_System.ServiceImpl.StudentServiceImpl;
 
 public class Projectutils {
-	private static StudentService stuservice =null;
+	private static StudentService stuservice = null;
 	private static BufferedReader br = null;
-	private static String emailrejex="[a-zA_Z0-9]+[@]{1}[a-zA_Z0-9]+[.]{1}[a-zA_Z0-9]{2,3}";
-	private static String mobilerejex="[789]{1}[0-9]{9}";
+	private static String emailrejex = "[a-zA_Z0-9]+[@]{1}[a-zA_Z0-9]+[.]{1}[a-zA_Z0-9]{2,3}";
+	private static String mobilerejex = "[789]{1}[0-9]{9}";
+
 	public Projectutils() {
-		stuservice= new StudentServiceImpl(); 
+		stuservice = new StudentServiceImpl();
 		br = new BufferedReader(new InputStreamReader(System.in));
 	}
-	public static boolean patternmatch(String pattern , String value) {
-		return Pattern.matches(pattern,value);
+
+	public static boolean patternmatch(String pattern, String value) {
+		return Pattern.matches(pattern, value);
 	}
+
+	public static String Scan(String msg) throws IOException {
+		System.out.println("\nEnter " + msg + " : ");
+		return br.readLine();
+	}
+
 	public static String ScanEmailid() throws IOException {
-		String email=new String();
-		while(true) {	
+		String email = new String();
+		while (true) {
 			System.out.println("Enter Email Id : ");
-			email=br.readLine();
-			if(patternmatch(emailrejex,email))
+			email = br.readLine();
+			if (patternmatch(emailrejex, email))
 				break;
-			else
-			{
+			else {
 				System.out.println("\nInvalid Email id !!!!!!!\n\n");
 			}
 		}
 		return email;
 	}
+
 	public static String Scanmobile() throws IOException {
-		String mobile=new String();
-		while(true) {
-			System.out.println("Enter  Mobile : ");
-			mobile=br.readLine();
-			if(patternmatch(mobilerejex, mobile)) {
+		String mobile = new String();
+		while (true) {
+			System.out.println("Enter  Mobile no : ");
+			mobile = br.readLine();
+			if (patternmatch(mobilerejex, mobile)) {
 				break;
-			}
-			else {
+			} else {
 				System.out.println("Unvalid Mobile Number!!!!!!!!!!!");
 			}
 		}
 		return mobile;
 	}
+
 	public static String Scangender() throws NumberFormatException, IOException {
-		String gender=new String();
-		while(true) {	
+		String gender = new String();
+		while (true) {
 			System.out.println("\nSelect Student Gender : \n1. Male\n2.Female\n");
-			int c1= Integer.parseInt(br.readLine());
-			if(c1==1) {
-				gender="Male";
+			int c1 = Integer.parseInt(br.readLine());
+			if (c1 == 1) {
+				gender = "Male";
 				break;
-			}
-			else if(c1==2) {
-				gender="Female";
+			} else if (c1 == 2) {
+				gender = "Female";
 				break;
-			}
-			else {
+			} else {
 				System.out.println("Invalid choice!!!!!\nChoose again!!!!!!!!!!");
 			}
 		}
 		return gender;
 	}
+
 	public void insertStudent() throws IOException {
-	
-		StudentDTO student=new StudentDTO();
+
+		StudentDTO student = new StudentDTO();
 		System.out.println("Student Details : ");
-		System.out.println("\n\nEnter Student id : ");
-		student.setStudent_id( Integer.parseInt(br.readLine()));
-		System.out.println("Enter Name : ");
-		student.setStudent_name(br.readLine());
-		System.out.println("Enter Address : ");
-		student.setStudent_address(br.readLine());
+		student.setStudent_id(Integer.parseInt(Scan("Student Id")));
+		student.setStudent_name(Scan("Student Name"));
+		student.setStudent_address("Student Address");
 		student.setStudent_email_id(ScanEmailid());
 		student.setStudent_gen(Scangender());
 		student.setStudent_mobile(Scanmobile());
 		System.out.println("Parent Details : \n");
-		System.out.println("Enter Student Parent Name : ");
-		student.setStudent_parent_name(br.readLine());
+		student.setStudent_parent_name(Scan("Student Parent Name"));
 		student.setStudent_parent_mobile(Scanmobile());
 		student.setStudent_parent_email_id(ScanEmailid());
-		System.out.println("Enter Reference Name : ");
-		student.setStudent_refname(br.readLine());
+		student.setStudent_refname(Scan("Reference Name"));
 		stuservice.insert(student);
 	}
-	
+
 	public void deleteStudent() throws NumberFormatException, IOException {
-		
+
 		System.out.println("\nEnter a Student Id : ");
-		int id = Integer.parseInt(br.readLine());
+		int id = Integer.parseInt(Scan("Student Id"));
 		stuservice.delete(id);
 	}
-	
+
 	public void updateStudent() throws IOException {
 		System.out.println("\nEnter a Student Id : ");
 		int id = Integer.parseInt(br.readLine());
 		findStudent(id);
 		System.out.println("\n");
-		StudentDTO student=new StudentDTO();
+		StudentDTO student = new StudentDTO();
 		student.setStudent_id(id);
-		System.out.println("Enter Name : ");
-		student.setStudent_name(br.readLine());
-		System.out.println("Enter Address : ");
-		student.setStudent_address(br.readLine());
+		System.out.println("Student Details : ");
+		student.setStudent_id(Integer.parseInt(Scan("Student Id")));
+		student.setStudent_name(Scan("Name"));
+		student.setStudent_address("Address");
 		student.setStudent_email_id(ScanEmailid());
 		student.setStudent_gen(Scangender());
 		student.setStudent_mobile(Scanmobile());
 		System.out.println("Parent Details : \n");
-		System.out.println("Enter Student Parent Name : ");
-		student.setStudent_parent_name(br.readLine());
+		student.setStudent_parent_name(Scan("Parent Name"));
 		student.setStudent_parent_mobile(Scanmobile());
 		student.setStudent_parent_email_id(ScanEmailid());
-		System.out.println("Enter Reference Name : ");
-		student.setStudent_refname(br.readLine());
+		student.setStudent_refname(Scan("Reference Name"));
 		stuservice.update(student);
 	}
-	
+
 	public void findStudent(int id) {
 		StudentDTO student = stuservice.find(id);
-		if(student == null) {
+		if (student == null) {
 			System.out.println("\n\nNot Found !!!!!!!!!!!!\n\n");
 			return;
 		}
-		System.out.println("\n"+student);
+		System.out.println("\n" + student);
 	}
-	
+
 	public void findAllStudent() {
-		
+
 		List<StudentDTO> students = stuservice.findAll();
-		if(students.size()==0) {
+		if (students.size() == 0) {
 			System.out.println("\n\nNo Students !!!!!!!!!!!!\n\n");
 			return;
 		}
 		System.out.println("Students : \n\n");
-		  for (int i=0; i<students.size(); i++)
-		  {
-			  StudentDTO student =  students.get(i);
-			  System.out.println(student + "\n"); 
-		  }
+		for (int i = 0; i < students.size(); i++) {
+			StudentDTO student = students.get(i);
+			System.out.println(student + "\n");
+		}
 	}
 }
